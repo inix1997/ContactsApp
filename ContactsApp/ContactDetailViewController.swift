e24eb2de586d032c08134caf98bd29c4137ba770
Ignacio Segui <ignanacho97@gmail.com>
2021-03-23 13:56:44 -0300

    Adding ui elements

--- ContactsApp/ContactDetailViewController.swift
+++ ContactsApp/ContactDetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ContactDetailViewController.swift
 //  ContactsApp
 //
 //  Created by Ignacio Segui on 23/03/2021.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ContactDetailViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
     }
 
-
 }
-
